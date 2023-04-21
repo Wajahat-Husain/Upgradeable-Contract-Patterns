0a4065360d2e4387945c719b8dd2072ef5c9d2b4
wajahat8888 <97318378+wajahat8888@users.noreply.github.com>
2023-04-21 22:42:33 +0500

    first push

---
+++ hardhat.config.js
@@ -0,0 +1,8 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-etherscan");
+
+module.exports = {
+  solidity: "0.8.17",
+};
